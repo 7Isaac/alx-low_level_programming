0cb103feb9496206dcd9a26ae7c88914e25edb77
7Isaac <isaacndekezi@gamil.com>
2022-11-14 19:40:34 -0800

    preprocessor

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#fndef _sum_h_
+
+#define _sum_h_
+
+
+
+#define SUM(x, y) ((x) + (y))
+
+
+
+#endif
---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+#include "1-pi.h"
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif
+
+
